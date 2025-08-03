3a3af7559c198e0ec4db253eb8127852019dfd78
TheGrizzlyDev <tonio9681@gmail.com>
2025-08-03 02:23:59 +0100

    split chunks and add an helpful analyse command

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+const shouldAnalyze = process.env.ANALYZE === 'true'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -27,10 +30,34 @@ export default defineConfig({
           }
         ]
       }
+    }),
+    shouldAnalyze && visualizer({
+      open: true, // automatically open report in browser
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'stats.html'
     })
-  ],
+  ].filter(Boolean),
   test: {
     environment: 'jsdom',
     globals: true
+  },
+  build: {
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@dice-roller/rpg-dice-roller')) {
+              return 'chunk-dice-roller'
+            }
+            if (id.includes('react-dom')) {
+              return 'chunk-react-dom'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
   }
 })
